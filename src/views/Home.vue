6bb76e8701643deaea7e393b27a244fe8ab847c3
EDEN <EDBEK@YAHOO.COM>
2020-05-05 21:48:51 -0500

    Got rid of excess routes from the nav bar and updated home page so that our names were no longer present

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h2>Hey there!</h2>
-    <h1>Hi from Tom!</h1>
-    <h2>hi from Eden...</h2>
   </div>
 </template>
 
@@ -13,7 +10,7 @@
 export default {
   data: function() {
     return {
-      message: "Welcome to Resumes!",
+      message: "Welcome to Your Resume Platform!",
     };
   },
   created: function() {},
