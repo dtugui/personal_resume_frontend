4e71b68f59cc647bfba2d0b18e8fd9466b2c47f7
EDEN <EDBEK@YAHOO.COM>
2020-05-05 21:15:59 -0500

    Resolved conflict with signup and logout

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,8 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
-<<<<<<< HEAD
 import StudentShow from "../views/StudentShow.vue";
-=======
 import Logout from "../views/Logout.vue";
->>>>>>> be32143e05b4fa2a6124fba6e884594e58bf77ca
 
 Vue.use(VueRouter);
 
@@ -27,11 +24,8 @@ const routes = [
   },
   { path: "/signup", name: "signup", component: Signup },
   { path: "/login", name: "login", component: Login },
-<<<<<<< HEAD
   { path: "/students/:id", name: "student-show", component: StudentShow },
-=======
   { path: "/logout", name: "logout", component: Logout },
->>>>>>> be32143e05b4fa2a6124fba6e884594e58bf77ca
 ];
 
 const router = new VueRouter({
